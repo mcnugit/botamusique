3bab9a32a29585f5c630c4b682b38d0d4853b467
mcnugit <mcnugit@gmail.com>
2025-05-19 07:12:58 -0700

    Always update whisper sessions on user changes

--- mumbleBot.py
+++ mumbleBot.py
@@ -230,12 +230,12 @@ class MumbleBot:
         assert var.config.get("bot", "when_nobody_in_channel") in ['pause', 'pause_resume', 'stop', 'nothing', ''], \
             "Unknown action for when_nobody_in_channel"
 
-        if var.config.get("bot", "when_nobody_in_channel") in ['pause', 'pause_resume', 'stop']:
-            user_change_callback = \
-                lambda user, action: threading.Thread(target=self.users_changed,
-                                                      args=(user, action), daemon=True).start()
-            self.mumble.callbacks.set_callback(pymumble.constants.PYMUMBLE_CLBK_USERREMOVED, user_change_callback)
-            self.mumble.callbacks.set_callback(pymumble.constants.PYMUMBLE_CLBK_USERUPDATED, user_change_callback)
+        # Always track user changes so whisper sessions stay updated after a listener reconnects
+        user_change_callback = \
+            lambda user, action: threading.Thread(target=self.users_changed,
+                                                  args=(user, action), daemon=True).start()
+        self.mumble.callbacks.set_callback(pymumble.constants.PYMUMBLE_CLBK_USERREMOVED, user_change_callback)
+        self.mumble.callbacks.set_callback(pymumble.constants.PYMUMBLE_CLBK_USERUPDATED, user_change_callback)
 
         # Debug use
         self._loop_status = 'Idle'
